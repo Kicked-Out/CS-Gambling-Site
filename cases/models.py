c77af2a22b0be34c1d7e731c4706744d4e2c2bb0
Denys Gladan <69466074+MrLazYT@users.noreply.github.com>
2025-02-02 14:46:15 +0200

    Added Api Support

--- cases/models.py
+++ cases/models.py
@@ -7,17 +7,9 @@ class Case(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
-class Weapon(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class Skin(models.Model):
     name = models.CharField(max_length=100)
-    weapon = ForeignKey(Weapon, on_delete=models.CASCADE, related_name='skins')
-    image = models.ImageField(upload_to=f'skins/{weapon.name}/')
-    case = ForeignKey(Case, on_delete=models.CASCADE, related_name='skins')
+    name_uk = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=10)
-    range = models.IntegerField()
     odds = models.DecimalField(decimal_places=3, max_digits=10)
+    case = ForeignKey(Case, on_delete=models.CASCADE, related_name='skins')
--- static/js/db_initializer.js
+++ static/js/db_initializer.js
@@ -1,25 +1,78 @@
-url = 'https://bymykel.github.io/CSGO-API/api/uk/skins.json'
+url = 'https://bymykel.github.io/CSGO-API/api/en/skins.json'
+url_uk = 'https://bymykel.github.io/CSGO-API/api/uk/skins.json'
 
-async function fetchApi() {
+const skinArr = []
+
+function pushSkin(skinData) {
+    const skin = {
+        name: skinData.name,
+        name_uk: "",
+        rarityName: skinData.rarity.name,
+        rarityColor: skinData.rarity.color,
+        image: skinData.image,
+        wearRating: undefined,
+        price: 0.00
+    };
+
+    skinArr.push(skin);
+}
+
+async function getAllSkins() {
     const response = await fetch(url);
     const json = await response.json();
 
     for (const key in json) {
-        sendWeaponAndSkinData(json[key])
+        pushSkin(json[key]);
     }
 }
 
-async function sendWeaponAndSkinData(skinData) {
-    fetch("https://localhost:8001/cases/add-skin/", {
-        method: 'POST',
-        body: JSON.stringify({
-            skinName: skinData.name,
-            weaponName: skinData.weapon.name,
-            rarityName: skinData.rarity.name,
-            rarityColor: skinData.rarity.color,
-            image: skinData.image,
-        }),
-    });
+function pushSkinUkName(skinIndex, UkSkinData) {
+    skinArr[skinIndex].name_uk = UkSkinData.name;
 }
 
-fetchApi();
+async function getAllSkinUkNames() {
+    const response = await fetch(url_uk);
+    const json = await response.json();
+
+    for (const key in json) {
+        pushSkinUkName(key, json[key]);
+    }
+}
+
+async function getSkinPrice(skin) {
+    const apiKey = "4wYD32slQWBHfEphJL2JyqkCL0YX054";
+    const wearRatings = ["Factory New", "Minimal Wear", "Field-Tested", "Well-Worn", "Battle-Scarred"]
+    const randomIndex = Math.floor(Math.random() * (wearRatings.length - 1));
+    const randomWear = wearRatings[randomIndex];
+    const url = `https://market.csgo.com/api/v2/search-item-by-hash-name?key=${apiKey}&hash_name=${skin.name} (${randomWear})`;
+    let data = undefined;
+
+    fetch(url)
+        .then(response => response.json().then(data => ({
+            data: data,
+            status: response.status
+        }))
+        .then(res => {
+            data = res.data.data[0];
+            skin.price = data.price / 1000;
+            skin.wearRating = randomWear;
+    }));
+}
+
+function getSkinByName(skinName) {
+    for (index in skinArr) {
+        if (skinArr[index].name === skinName) {
+            getSkinPrice(skinArr[index]);
+            console.log(skinArr[index]);
+            break;
+        }
+    }
+}
+
+async function initialize() {
+    await getAllSkins();
+    await getAllSkinUkNames();
+    getSkinByName("AK-47 | Nightwish");
+}
+
+initialize();
