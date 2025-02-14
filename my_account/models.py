82e611be7fb5e1c4887763abbc9cd3637075cf27
Denys Gladan <69466074+MrLazYT@users.noreply.github.com>
2025-02-14 22:17:08 +0200

    added upgrader page v1

--- my_account/models.py
+++ my_account/models.py
@@ -12,7 +12,7 @@ class Profile(AbstractUser):
         return self.username
 
 class InventoryItem(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='inventory')
     item_name = models.CharField(max_length=255)
     item_value = models.DecimalField(max_digits=10, decimal_places=2)
 
--- upgrader/views.py
+++ upgrader/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def upgrade(request):
-    return render(request, 'upgrade/upgrade.html')
+    user = request.user
+    inventoryItems = None
+
+    if (user.is_authenticated):
+        inventoryItems = user.inventory
+
+    return render(request, 'upgrade/upgrade.html', {'inventoryШtems': inventoryItems})
