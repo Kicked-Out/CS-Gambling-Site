64b18a5e5097579ec4f89cd4677c68dc58ec9c78
GeorgHerych <skayday44@gmail.com>
2025-02-23 19:25:36 +0200

    add_chat_exchanger_frendship

--- exchanger/apps.py
+++ exchanger/apps.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
-
 
 class ExchangerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exchanger'
-=======
-from django.apps import AppConfig
-
-
-class ExchangerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'exchanger'
->>>>>>> 587eeefc3f2ad2c4ea3190741cdaa5f9b6d795f9
